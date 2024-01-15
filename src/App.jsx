8155eb4703c92078667087ee8f902b58be9bdf5d
Tushar <traghav64@gmail.com>
2024-01-15 11:32:38 +0530

    .

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+    const [users, setUsers] = useState([]);
+
+    // use useEffect to fetch user Data and then render it on the page
+
+    return <div className="app">{/* Render data here */}</div>;
+}
+
+export default App;
